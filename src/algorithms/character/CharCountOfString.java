9c04d6bec12522c3e5c63d0be57308cd7b0d754b
Harry Xu <harry.xv@gmail.com>
2025-03-07 19:16:02 -0500

    add count chars in s to map implementation

--- src/algorithms/character/CharCountOfString.java
+++ src/algorithms/character/CharCountOfString.java
@@ -1,13 +1,21 @@
 package algorithms.character;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * count frequency of characters in lowercase for a given string s, using Array
  * the index of the array is implicitly by character.
  */
 public class CharCountOfString {
-    public static int[] countChars(String s) {
+    public static Map<Character, Integer> countCharsToMap(String s) {
+        Map<Character, Integer> charCount = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            charCount.put(s.charAt(i), charCount.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        return charCount;
+    }
+
+    public static int[] countCharsToArray(String s) {
         int[] charCount = new int[26]; //ASCII max numbers, index 0 : 'a', default value is 0
         for (char c : s.toCharArray()) {
             if (Character.isLetter(c)) {
@@ -19,8 +27,9 @@ public class CharCountOfString {
     }
 
     public static void main(String[] args) {
-        String s = "This is an exciting news";
-        System.out.println(Arrays.toString(countChars(s))); //expecting {1,0,1,0....1,0,0]
+        String s = "Thisisanexcitingnews";
+        System.out.println(Arrays.toString(countCharsToArray(s))); //expecting {1,0,1,0....1,0,0]
+        System.out.println(countCharsToMap(s)); //expecting {1,0,1,0....1,0,0]
         //result: [1, 0, 1, 0, 2, 0, 1, 1, 4, 0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 2, 0, 0, 1, 1, 0, 0]
     }
 }
