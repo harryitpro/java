32a7c5e0f8e90dc576464f85e543bdcfff4fbfe2
Harry Xu <harry.xv@gmail.com>
2025-03-11 17:18:04 -0400

    add character frequency counter for ascii

---
+++ src/algorithms/fundamental/CharFrequencyCounter.java
@@ -0,0 +1,19 @@
+package algorithms.fundamental;
+
+/**
+ * alternative way of Frequency Counter other than the Map<Integer,Integer> freq
+ */
+public class CharFrequencyCounter {
+    static int[] countEachCharFrequency(String s) {
+        int[] freq = new int[128];
+        for (char c : s.toCharArray()) {
+            freq[c] = freq[c] + 1;
+        }
+        return freq;
+    }
+
+    public static void main(String[] args) {
+        String s = "This is CharFrequencyCounter app";
+        System.out.println(countEachCharFrequency(s)[84]); //1  "T"  occurs 1
+    }
+}
