a7d1196061efaab28556a9a5e406a38af2fe3467
Harry Xu <harry.xv@gmail.com>
2025-04-06 23:24:30 -0400

    update and add new practice

--- src/algorithms/fundamental/FrequentlyBuyTogetherRecommendation.java
+++ src/algorithms/fundamental/FrequentlyBuyTogetherRecommendation.java
@@ -1,12 +1,15 @@
 package algorithms.fundamental;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
  * explanation:
  * Map<String, Map<String, Integer>>  K: item, V: co-purchased items and their corresponding frequency.
- * for given itme, sort its co-purchased items by frequency desc. return the limit item names.
+ * for given item, sort its co-purchased items by frequency desc. return the limit item names.
  */
 public class FrequentlyBuyTogetherRecommendation {
     private static List<String> recommendItem(List<List<String>> orders, String item, int limit) {
@@ -19,13 +22,19 @@ public class FrequentlyBuyTogetherRecommendation {
         return getRecommendItems(graph, item, limit);
     }
 
-    private static void addEdgesByList(
+    /**
+     * build order history graph by adding orders.
+     *
+     * @param graph   orderHistory, represent in graph data model.
+     * @param orders: orders resource data.
+     */
+    static void addEdgesByList(
             Map<String, Map<String, Integer>> graph, List<List<String>> orders) {
-        for (List<String> order : orders) {
-            for (String item : order) {
+        for (List<String> orderItems : orders) {
+            for (String item : orderItems) {
                 graph.put(item, graph.getOrDefault(item, new HashMap<>()));
                 Map<String, Integer> coPurchaseItemMap = graph.get(item);
-                order.stream()
+                orderItems.stream()
                         .filter(orderItem -> !orderItem.equals(item))
                         .forEach(orderItem -> coPurchaseItemMap.put(orderItem, coPurchaseItemMap.getOrDefault(orderItem, 0) + 1));
 
@@ -33,14 +42,23 @@ public class FrequentlyBuyTogetherRecommendation {
         }
     }
 
-    private static List<String> getRecommendItems(
+    /**
+     * @param graph orderHistory graph.
+     * @param item: given item for recommendations.
+     * @param limit
+     * @return
+     */
+    static List<String> getRecommendItems(
             Map<String, Map<String, Integer>> graph, String item, int limit) {
         Map<String, Integer> coPurchasedItemFrequency = graph.get(item);
-        //presume it's not empty
+
+        if (coPurchasedItemFrequency == null)
+            return new ArrayList<>();
+
         return coPurchasedItemFrequency.entrySet().stream()
                 .sorted((entry1, entry2) -> entry2.getValue() - entry1.getValue())
-                .map(entry -> entry.getKey())
                 .limit(limit)
+                .map(entry -> entry.getKey())
                 .collect(Collectors.toList());
     }
 
