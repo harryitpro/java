d51f134e902406bc6bf0e1c042e4890e8c802409
Harry Xu <harry.xv@gmail.com>
2025-04-08 10:36:41 -0400

    update algorithms

--- src/algorithms/fundamental/commonquestions/AnagramGroups.java
+++ src/algorithms/fundamental/commonquestions/AnagramGroups.java
@@ -7,7 +7,19 @@ import java.util.*;
  * anagram words should be the same.
  */
 public class AnagramGroups {
-    public static List<List<String>> findAnagrams(String[] words) {
+    static Set<Map<Character, Integer>> findAnagramsToSet(String[] words) {
+        Set<Map<Character, Integer>> groupSet = new HashSet<>();
+        for (String s : words) {
+            Map<Character, Integer> charFreq = new HashMap<>();
+            for (char c : s.toCharArray()) {
+                charFreq.put(c, charFreq.getOrDefault(c, 0) + 1);
+            }
+            groupSet.add(charFreq);
+        }
+        return groupSet;
+    }
+
+    public static Map<String, List<String>> findAnagrams(String[] words) {
         Map<String, List<String>> anagramMap = new HashMap<>();
 
         for (String word : words) {
@@ -18,20 +30,13 @@ public class AnagramGroups {
             // Add to HashMap
             anagramMap.computeIfAbsent(sortedWord, k -> new ArrayList<>()).add(word);
         }
-
-        // Collect groups of anagrams (only if more than 1 word)
-        List<List<String>> result = new ArrayList<>();
-        for (List<String> group : anagramMap.values()) {
-            if (group.size() > 1) {
-                result.add(group);
-            }
-        }
-        return result;
+        return anagramMap;
     }
 
     public static void main(String[] args) {
         String[] words = {"listen", "silent", "enlist", "rat", "tar", "art", "evil", "vile", "live", "veil"};
-        System.out.println(findAnagrams(words));
+        System.out.println(findAnagrams(words).size()); //3 groups
+        System.out.println(findAnagramsToSet(words).size());
     }
 }
 
